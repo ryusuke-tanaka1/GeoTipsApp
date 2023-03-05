568895b11add20fef398584da45f0ea7d29d4500
tanakar <ryusuke1133@gmail.com>
2023-03-05 19:47:15 +0900

    finish SessionsNew

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
+
+  get '/login', to: 'Sessions#new'
+  post '/login', to: 'Sessions#create'
+  delete '/logout', to: 'Sessions#destroy'
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
