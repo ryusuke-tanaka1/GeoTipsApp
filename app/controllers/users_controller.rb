88c9f5110edf305b36329c903125ec1061153316
tanakar <ryusuke1133@gmail.com>
2023-03-06 03:44:31 +0900

    ユーザーview作成、タイトル変更対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :delete]
+
+  def show
+  end
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save!
+    else
+    end
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+    def set_user
+      @user = User.find(:id)
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def create_title(page_title = "")
+    app_title = "GeoTips"
+    if page_title.blank?
+      app_title
+    else
+      page_title + "|" + app_title
+    end
+  end
 end
