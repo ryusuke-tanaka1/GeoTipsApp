889b588c19bf6bb5e8b068bebb8a099dba9da66e
tanakar <ryusuke1133@gmail.com>
2023-04-06 19:31:48 +0900

    before like-button

--- app/uploaders/img_uploader_uploader.rb
+++ app/uploaders/img_uploader_uploader.rb
@@ -3,10 +3,6 @@ class ImgUploaderUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url
-    '【4K対応PC版】2023年4月メンバー限定壁紙.jpg'
-  end
-
   def extension_allowlist
     %w(jpg jpeg gif png)
   end
